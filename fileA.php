b94d5cb4132c138d1bcdfda5af48b658942d7538
t-taira <t-taira@musu-b.com>
2023-05-09 14:55:01 +0900

    コメント整理

--- fileA.php
+++ fileA.php
@@ -1,10 +1,1 @@
 <?php
-// rebaseテストAで実装
-// rebaseテストAで実装2
-// rebaseテストAで実装3
-// rebaseテストAで実装4
-
-// mergeテストA実装1
-// mergeテストA実装2
-// mergeテストA実装3
-// mergeテストA実装4
--- fileB.php
+++ fileB.php
@@ -1,6 +1,1 @@
 <?php
-// rebaseテストBで実装
-// rebaseテストBで実装2
-
-// mergeテストB実装1
-// mergeテストB実装2
